f26d1f87ba8d70f201823d7873936f0841c40f58
Bedashree Aparna Sahoo <aparna.bedashree@gmail.com>
2025-04-26 17:40:28 +0530

    polishing of the next.js app.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <Layout>
       <Head>
         <title>First Post</title>
+        <script src="https://connect.facebook.net/en_US/sdk.js" />
       </Head>
       <h1>First Post</h1>
       <h2>
